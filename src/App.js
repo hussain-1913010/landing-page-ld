ff49bbe3f11a265254adf75ded2622039353560e
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-10-10 10:01:51 +0600

    Intro initialized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import './App.css';
+import Intro from './components/Intro';
 import Navbar from './components/Navbar';
 
 const Container = styled.div`
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Container>
         <Navbar/>
+        <Intro/>
       </Container>
     </div>
   );
