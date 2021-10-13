d9e89751dcf82d19f9c1f9b38ebc312c4af9446e
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-10-13 16:04:54 +0600

    clip path done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import styled from 'styled-components';
-import './App.css';
-import Intro from './components/Intro';
-import Navbar from './components/Navbar';
+import styled from "styled-components";
+import "./App.css";
+import Intro from "./components/Intro";
+import Navbar from "./components/Navbar";
 
 const Container = styled.div`
   height: 100vh;
   overflow: hidden;
-`
+
+`;
+
+const IntroShape = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  clip-path: polygon(63% 0, 100% 0, 100% 100%, 54% 100%);
+
+  z-index: -1;
+  background-color: crimson;
+`;
 function App() {
   return (
     <div className="App">
       <Container>
-        <Navbar/>
-        <Intro/>
+        <Navbar />
+        <Intro />
+        <IntroShape/>
       </Container>
     </div>
   );
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import girl from './../img/girl_bgless.png';
+import girl from './../img/lady.png';
 
 const Container = styled.div`
   height: calc(100vh - 50px);
